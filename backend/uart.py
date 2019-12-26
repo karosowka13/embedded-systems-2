92141a059a18b6ca64194acc6c21cb779f502528
Stanislaw Cabala <stanislaw.cabala@gmail.com>
2019-12-26 17:15:12 +0100

    Add implementation of UART class

--- backend/uart.py
+++ backend/uart.py
@@ -1,17 +1,57 @@
-import serial
+from serial import Serial, SerialTimeoutException
 
 
 class UART:
     TASKS = (1, 2, 3)
 
+    def __init__(self, port, speed=9600, timeout=1.0):
+        self.ser = Serial(port, baudrate=speed, timeout=timeout)
+
+    def __del__(self):
+        if self.ser.is_open:
+            self.ser.close()
+        del self.ser
+
+    def write(self, msg):
+        self.ser.write(msg.encode())
+
+    def read(self, msg):
+        self.write(msg)
+        try:
+            return self.ser.read(1).decode()
+        except SerialTimeoutException:
+            return "-2"
+
     def manage_task(self, idx, args):
-        return 0
+        priority = args.get("priority")
+        duration = args.get("duration")
+        rc = 0
+        if priority is not None:
+            rc += int(self.read(f"cpt{idx} {priority}"))
+        if duration is not None:
+            rc += int(self.read(f"cdt{idx} {duration}"))
+        return rc
 
     def manage_timer(self, args):
-        return 0
+        control = args.get("running")
+        period = args.get("period")
+        semaphore = args.get("semaphore")
+        rc = 0
+        if control is not None:
+            if control is True:
+                self.write("sta_t")
+            else:
+                self.write("sto_t")
+        if period is not None:
+            rc += int(self.read(f"tp {period}"))
+        if semaphore is not None:
+            rc += int(self.read(f"tc {semaphore}"))
+        return rc
 
-    def queue_put(self, value):
-        return 0
+    def queue_put(self, args):
+        value = args.get("value")
+        if value is not None:
+            rc = int(self.read(f"pq {value}"))
 
     def get_semaphore(self, idx):
         pass

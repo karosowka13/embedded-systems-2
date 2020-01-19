293f59222879307d4b579c64f818258a7cb9ecdf
Piotr Kozimor <p1996k@gmail.com>
2020-01-19 22:53:17 +0100

    Edit backend tests

--- backend/test/write.py
+++ backend/test/write.py
@@ -1,10 +1,13 @@
+from time import sleep
 from uart import UART
 
 u = UART('/dev/ttyACM0', speed=115200)
 # u.write('cdt1 200'.ljust(12))
 # u.write('cpt1 10'.ljust(12))
 # u.write('tp 2000'.ljust(12))
-# u.write('gs 4'.ljust(12))
 # u.write('pq 10'.ljust(12))
-print(u.read('sta_t 0'.ljust(12)))
+# print(u.read('sta_t 0'.ljust(12)))
 # print(u.read('pq 10'.ljust(12)))
+for i in range(10):
+    u.write('gs 4'.ljust(12))
+    sleep(0.05)

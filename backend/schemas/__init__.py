18c5652d76bbdc764bd5ad2f750a2cdd6015340a
Stanislaw Cabala <stanislaw.cabala@gmail.com>
2020-01-20 12:42:03 +0100

    Add semaphore json schema

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -12,3 +12,6 @@ with (_base_dir / "timerschema.json").open("r") as fp:
 
 with (_base_dir / "queueschema.json").open("r") as fp:
     queue_schema = json.load(fp)
+
+with (_base_dir / "semaphoreschema.json").open("r") as fp:
+    semaphore_schema = json.load(fp)

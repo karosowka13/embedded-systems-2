3d272c7d24d2f1cd8e41e7a96abfc0624303adfe
Stanislaw Cabala <stanislaw.cabala@gmail.com>
2020-02-01 16:14:45 +0100

    Add base class for serial connection

---
+++ backend/uart/serial.py
@@ -0,0 +1,34 @@
+from serial_asyncio import create_serial_connection
+
+
+class UART:
+    TASKS = (1, 2, 3)
+
+    def __init__(self, port, speed=9600, debug=False):
+        self.port = port
+        self.speed = speed
+        self.debug = debug
+        self._reader = None
+        self._writer = None
+
+    # async def __del__(self):
+    #     await self.close()
+
+    async def open(self):
+        self._reader, self._writer = await create_serial_connection(port=self.port, baudrate=self.speed)
+
+    async def close(self):
+        self._writer.close()
+        await self._writer.wait_closed()
+        self._reader = None
+        self._writer = None
+
+    def write(self, msg):
+        if self._writer is not None:
+            self._writer.write(msg.encode("ascii"))
+
+    def read(self, msg):
+        if self._reader is not None:
+            self.write(msg)
+            return self._reader.readline().decode("ascii")
+        return ""

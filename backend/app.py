0542b8280982e0273a6e5eb10836a186092fe7f9
Stanislaw Cabala <stanislaw.cabala@gmail.com>
2020-02-01 16:49:44 +0100

    Move connecting to UART device into app cleanup context

--- backend/app.py
+++ backend/app.py
@@ -5,13 +5,21 @@ from . import config
 from .routes import routes
 
 
+async def uart_conn(app):
+    app["uart"] = UART(
+        config.SERIAL_PORT,
+        speed=config.SERIAL_SPEED,
+        debug=config.APP_DEBUG,
+    )
+    await app["uart"].open()
+    yield
+    await app["uart"].close()
+
+
 app = web.Application(debug=config.APP_DEBUG)
 app["port"] = config.APP_PORT
 app["host"] = config.APP_HOST
-app["uart"] = UART(
-    config.SERIAL_PORT,
-    speed=config.SERIAL_SPEED,
-    debug=config.APP_DEBUG,
-)
+
+app.cleanup_ctx.append(uart_conn)
 
 app.add_routes(routes)

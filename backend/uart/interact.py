c73e4469741bdbfba89489ebbc656aa1f3e872e7
Stanislaw Cabala <stanislaw.cabala@gmail.com>
2020-02-05 17:42:21 +0100

    Improve interactive client

--- backend/uart/interact.py
+++ backend/uart/interact.py
@@ -4,8 +4,8 @@ import sys
 from .serial import Serial
 
 
-async def main(port):
-    s = Serial(port)
+async def main(port, speed):
+    s = Serial(port, speed=speed)
     print(f"Connecting to {port}...")
     await s.open()
     print("Connected")
@@ -13,12 +13,14 @@ async def main(port):
     try:
         while True:
             msg = input(">>>")
-            ret = s.write(msg)
-            print(repr(ret))
+            ret = s.write(msg.ljust(12))
+            if ret is not None:
+                print(f"Return message: {ret!r}")
     except KeyboardInterrupt:
         await s.close()
 
 
 if __name__ == '__main__':
     port = sys.argv[1]
-    asyncio.get_event_loop().run_until_complete(main(port))
+    speed = int(sys.argv[2])
+    asyncio.get_event_loop().run_until_complete(main(port, speed))
